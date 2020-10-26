d2bfd60cb8f09415501943b4dc80ac63146ad709
Zhao <zlin20@stuy.edu>
2020-10-26 18:50:57 -0400

    added test arrays for Tester

--- Tester.java
+++ Tester.java
@@ -1,5 +1,17 @@
 public class Tester{
+  import java.util.Arrays;
   public static void main(String[] args){
-
+    int[][]  A  =  {  {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 }
+               };
+    int[]  B  =   {  1, 3, 5 };
+    int[][] C = {{1,  2, 3, 4 },
+                  {  2, 3,  4,  1},
+                  { 3, 4,  1, 2 } };
+    int[][] D = { {  1,  1, 1 },
+                  {  2, 2, 2 },
+                  { 3,  3, 7 } };
+    int[][] E = { {  2,  4, 2 }, {  2, 2, 2 } };
   }
 }
